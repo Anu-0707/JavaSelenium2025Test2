c59242efd39a108f2bdb8cac863ce2ee2c218097
Anushree <anushree07p@gmail.com>
2025-07-20 12:12:15 +0530

    resolve conflit

--- src/p1/class1.java
+++ src/p1/class1.java
@@ -3,9 +3,15 @@ package p1;
 public class class1 {
 
 	public static void main(String[] args) {
+<<<<<<< Updated upstream
 		System.out.println("P1 c1 world");
 		System.out.println("Hi");
         
+=======
+		System.out.println("P1 c1 world ABC");
+		System.out.println("Hi there");
+
+>>>>>>> Stashed changes
 	}
 
 }
